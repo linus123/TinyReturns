4f12fcaf54e1d3a7eec85ed6ee944355157ddbe5
Paul Herera <pherrera@dimensional.com>
2017-05-12 07:59:04 -0500

    Changed build files

--- data/mssql/TinyReturns/Up/0002_AddTestData.EVN.LOCAL.sql
+++ data/mssql/TinyReturns/Up/0002_AddTestData.EVN.LOCAL.sql
@@ -1,106 +1,106 @@
-INSERT [Portfolio].[Benchmark] ([Number], [Name]) VALUES (10000, N'Benchmark 00')
+INSERT [Portfolio].[Benchmark] ([Number], [Name]) VALUES (10000, N'Russell 2000 Index')
 GO
-INSERT [Portfolio].[Benchmark] ([Number], [Name]) VALUES (10001, N'Benchmark 01')
+INSERT [Portfolio].[Benchmark] ([Number], [Name]) VALUES (10001, N'Russell 3000 Growth Index')
 GO
-INSERT [Portfolio].[Benchmark] ([Number], [Name]) VALUES (10002, N'Benchmark 02')
+INSERT [Portfolio].[Benchmark] ([Number], [Name]) VALUES (10002, N'Russell 2500 Index')
 GO
-INSERT [Portfolio].[Benchmark] ([Number], [Name]) VALUES (10003, N'Benchmark 03')
+INSERT [Portfolio].[Benchmark] ([Number], [Name]) VALUES (10003, N'S&P MidCap 400 Index')
 GO
-INSERT [Portfolio].[Benchmark] ([Number], [Name]) VALUES (10004, N'Benchmark 04')
+INSERT [Portfolio].[Benchmark] ([Number], [Name]) VALUES (10004, N'BofA Merrill Lynch 1-3 Year US Treasury and Agency Index')
 GO
-INSERT [Portfolio].[Benchmark] ([Number], [Name]) VALUES (10005, N'Benchmark 05')
+INSERT [Portfolio].[Benchmark] ([Number], [Name]) VALUES (10005, N'BofA Merrill Lynch Three-Month US Treasury Bill Index')
 GO
-INSERT [Portfolio].[Benchmark] ([Number], [Name]) VALUES (10006, N'Benchmark 06')
+INSERT [Portfolio].[Benchmark] ([Number], [Name]) VALUES (10006, N'Dow Jones US Select REIT Index')
 GO
-INSERT [Portfolio].[Benchmark] ([Number], [Name]) VALUES (10007, N'Benchmark 07')
+INSERT [Portfolio].[Benchmark] ([Number], [Name]) VALUES (10007, N'Japanese Large Companies (MSCI/Nomura)')
 GO
-INSERT [Portfolio].[Benchmark] ([Number], [Name]) VALUES (10008, N'Benchmark 08')
+INSERT [Portfolio].[Benchmark] ([Number], [Name]) VALUES (10008, N'Fama/French US Large Value Research Index')
 GO
-INSERT [Portfolio].[Benchmark] ([Number], [Name]) VALUES (10009, N'Benchmark 09')
+INSERT [Portfolio].[Benchmark] ([Number], [Name]) VALUES (10009, N'MSCI Europe ex UK Index (gross div.)')
 GO
-INSERT [Portfolio].[Benchmark] ([Number], [Name]) VALUES (10010, N'Benchmark 10')
+INSERT [Portfolio].[Benchmark] ([Number], [Name]) VALUES (10010, N'MSCI Pacific ex Japan Index (gross div.)')
 GO
-INSERT [Portfolio].[Benchmark] ([Number], [Name]) VALUES (10011, N'Benchmark 11')
+INSERT [Portfolio].[Benchmark] ([Number], [Name]) VALUES (10011, N'Russell Midcap Value Index')
 GO
-INSERT [Portfolio].[Benchmark] ([Number], [Name]) VALUES (10012, N'Benchmark 12')
+INSERT [Portfolio].[Benchmark] ([Number], [Name]) VALUES (10012, N'MSCI Emerging Markets Value Index (gross div.)')
 GO
-INSERT [Portfolio].[Benchmark] ([Number], [Name]) VALUES (10013, N'Benchmark 13')
+INSERT [Portfolio].[Benchmark] ([Number], [Name]) VALUES (10013, N'MSCI UK Small Cap Index (price-only)')
 GO
-INSERT [Portfolio].[Benchmark] ([Number], [Name]) VALUES (10014, N'Benchmark 14')
+INSERT [Portfolio].[Benchmark] ([Number], [Name]) VALUES (10014, N'Wilshire Micro Cap Index')
 GO
-INSERT [Portfolio].[Benchmark] ([Number], [Name]) VALUES (10015, N'Benchmark 15')
+INSERT [Portfolio].[Benchmark] ([Number], [Name]) VALUES (10015, N'MSCI Emerging Markets Index (gross div.)')
 GO
-INSERT [Portfolio].[Benchmark] ([Number], [Name]) VALUES (10016, N'Benchmark 16')
+INSERT [Portfolio].[Benchmark] ([Number], [Name]) VALUES (10016, N'MSCI Europe Small Cap Index (net div.)')
 GO
-INSERT [Portfolio].[Benchmark] ([Number], [Name]) VALUES (10017, N'Benchmark 17')
+INSERT [Portfolio].[Benchmark] ([Number], [Name]) VALUES (10017, N'MSCI EAFE Small Cap Index (net div.)')
 GO
-INSERT [Portfolio].[Benchmark] ([Number], [Name]) VALUES (10018, N'Benchmark 18')
+INSERT [Portfolio].[Benchmark] ([Number], [Name]) VALUES (10018, N'MSCI UK Small Cap Index (net div.)')
 GO
-INSERT [Portfolio].[Benchmark] ([Number], [Name]) VALUES (10019, N'Benchmark 19')
+INSERT [Portfolio].[Benchmark] ([Number], [Name]) VALUES (10019, N'MSCI World Index (net div.)')
 GO
-INSERT [Portfolio].[Benchmark] ([Number], [Name]) VALUES (10020, N'Benchmark 20')
+INSERT [Portfolio].[Benchmark] ([Number], [Name]) VALUES (10020, N'FTSE TMX Canada Universe Bond Index')
 GO
-INSERT [Portfolio].[Benchmark] ([Number], [Name]) VALUES (10021, N'Benchmark 21')
+INSERT [Portfolio].[Benchmark] ([Number], [Name]) VALUES (10021, N'Global 25/75 Composite Index (MSCI/Bloomberg Barclays)')
 GO
-INSERT [Portfolio].[Benchmark] ([Number], [Name]) VALUES (10022, N'Benchmark 22')
+INSERT [Portfolio].[Benchmark] ([Number], [Name]) VALUES (10022, N'Global 60/40 Composite Index (MSCI/Bloomberg Barclays)')
 GO
-INSERT [Portfolio].[Benchmark] ([Number], [Name]) VALUES (10023, N'Benchmark 23')
+INSERT [Portfolio].[Benchmark] ([Number], [Name]) VALUES (10023, N'MSCI World ex UK Value Index (net div.)')
 GO
-INSERT [Portfolio].[Benchmark] ([Number], [Name]) VALUES (10024, N'Benchmark 24')
+INSERT [Portfolio].[Benchmark] ([Number], [Name]) VALUES (10024, N'JP Morgan Global Government Bond Index Hedged in AUD 1-5 Years')
 GO
-INSERT [Portfolio].[Benchmark] ([Number], [Name]) VALUES (10025, N'Benchmark 25')
+INSERT [Portfolio].[Benchmark] ([Number], [Name]) VALUES (10025, N'MSCI Europe ex UK Index (net div.)')
 GO
-INSERT [Portfolio].[Benchmark] ([Number], [Name]) VALUES (10026, N'Benchmark 26')
+INSERT [Portfolio].[Benchmark] ([Number], [Name]) VALUES (10026, N'S&P EPAC SmallCap Value Index (Gross Div.)')
 GO
-INSERT [Portfolio].[Benchmark] ([Number], [Name]) VALUES (10027, N'Benchmark 27')
+INSERT [Portfolio].[Benchmark] ([Number], [Name]) VALUES (10027, N'FTSE North America Small Cap ex REITS Index')
 GO
-INSERT [Portfolio].[Benchmark] ([Number], [Name]) VALUES (10028, N'Benchmark 28')
+INSERT [Portfolio].[Benchmark] ([Number], [Name]) VALUES (10028, N'MSCI UK Small Cap Index (net div.)')
 GO
-INSERT [Portfolio].[Benchmark] ([Number], [Name]) VALUES (10029, N'Benchmark 29')
+INSERT [Portfolio].[Benchmark] ([Number], [Name]) VALUES (10029, N'Russell 2000 ex REIT Index')
 GO
-INSERT [Portfolio].[Benchmark] ([Number], [Name]) VALUES (10030, N'Benchmark 30')
+INSERT [Portfolio].[Benchmark] ([Number], [Name]) VALUES (10030, N'Russell Microcap Index')
 GO
-INSERT [Portfolio].[Portfolio] ([Number], [Name], [InceptionDate], [CloseDate]) VALUES (100, N'Portfolio 100', CAST(N'2000-01-08 00:00:00.000' AS DateTime), NULL)
+INSERT [Portfolio].[Portfolio] ([Number], [Name], [InceptionDate], [CloseDate]) VALUES (100, N'U.S. Micro Cap Portfolio-Institutional Class', CAST(N'2000-01-08 00:00:00.000' AS DateTime), NULL)
 GO
-INSERT [Portfolio].[Portfolio] ([Number], [Name], [InceptionDate], [CloseDate]) VALUES (101, N'Portfolio 101', CAST(N'2010-08-09 00:00:00.000' AS DateTime), NULL)
+INSERT [Portfolio].[Portfolio] ([Number], [Name], [InceptionDate], [CloseDate]) VALUES (101, N'Small Cap Value Subtrust', CAST(N'2010-08-09 00:00:00.000' AS DateTime), NULL)
 GO
-INSERT [Portfolio].[Portfolio] ([Number], [Name], [InceptionDate], [CloseDate]) VALUES (102, N'Portfolio 102', CAST(N'2013-01-03 00:00:00.000' AS DateTime), NULL)
+INSERT [Portfolio].[Portfolio] ([Number], [Name], [InceptionDate], [CloseDate]) VALUES (102, N'U.S. Large Cap Value Series', CAST(N'2013-01-03 00:00:00.000' AS DateTime), NULL)
 GO
-INSERT [Portfolio].[Portfolio] ([Number], [Name], [InceptionDate], [CloseDate]) VALUES (103, N'Portfolio 103', CAST(N'1985-12-30 00:00:00.000' AS DateTime), NULL)
+INSERT [Portfolio].[Portfolio] ([Number], [Name], [InceptionDate], [CloseDate]) VALUES (103, N'Stone Island Fire Five Year Variable', CAST(N'1985-12-30 00:00:00.000' AS DateTime), NULL)
 GO
-INSERT [Portfolio].[Portfolio] ([Number], [Name], [InceptionDate], [CloseDate]) VALUES (104, N'Portfolio 104', CAST(N'2000-08-15 00:00:00.000' AS DateTime), CAST(N'2017-12-05 00:00:00.000' AS DateTime))
+INSERT [Portfolio].[Portfolio] ([Number], [Name], [InceptionDate], [CloseDate]) VALUES (104, N'U.S. Large Cap Value Portfolio III', CAST(N'2000-08-15 00:00:00.000' AS DateTime), CAST(N'2017-12-05 00:00:00.000' AS DateTime))
 GO
-INSERT [Portfolio].[Portfolio] ([Number], [Name], [InceptionDate], [CloseDate]) VALUES (105, N'Portfolio 105', CAST(N'1999-07-03 00:00:00.000' AS DateTime), NULL)
+INSERT [Portfolio].[Portfolio] ([Number], [Name], [InceptionDate], [CloseDate]) VALUES (105, N'AB International Value Portfolio', CAST(N'1999-07-03 00:00:00.000' AS DateTime), NULL)
 GO
-INSERT [Portfolio].[Portfolio] ([Number], [Name], [InceptionDate], [CloseDate]) VALUES (106, N'Portfolio 106', CAST(N'2003-04-09 00:00:00.000' AS DateTime), NULL)
+INSERT [Portfolio].[Portfolio] ([Number], [Name], [InceptionDate], [CloseDate]) VALUES (106, N'U.S. Targeted Value Portfolio - Institutional Class', CAST(N'2003-04-09 00:00:00.000' AS DateTime), NULL)
 GO
-INSERT [Portfolio].[Portfolio] ([Number], [Name], [InceptionDate], [CloseDate]) VALUES (107, N'Portfolio 107', CAST(N'2010-08-09 00:00:00.000' AS DateTime), NULL)
+INSERT [Portfolio].[Portfolio] ([Number], [Name], [InceptionDate], [CloseDate]) VALUES (107, N'Dimensional Middle Earth Large Company Trust', CAST(N'2010-08-09 00:00:00.000' AS DateTime), NULL)
 GO
-INSERT [Portfolio].[Portfolio] ([Number], [Name], [InceptionDate], [CloseDate]) VALUES (108, N'Portfolio 108', CAST(N'2013-01-03 00:00:00.000' AS DateTime), NULL)
+INSERT [Portfolio].[Portfolio] ([Number], [Name], [InceptionDate], [CloseDate]) VALUES (108, N'Dimensional Global Value Trust', CAST(N'2013-01-03 00:00:00.000' AS DateTime), NULL)
 GO
-INSERT [Portfolio].[Portfolio] ([Number], [Name], [InceptionDate], [CloseDate]) VALUES (109, N'Portfolio 109', CAST(N'1985-12-30 00:00:00.000' AS DateTime), NULL)
+INSERT [Portfolio].[Portfolio] ([Number], [Name], [InceptionDate], [CloseDate]) VALUES (109, N'XX International Value Fund', CAST(N'1985-12-30 00:00:00.000' AS DateTime), NULL)
 GO
-INSERT [Portfolio].[Portfolio] ([Number], [Name], [InceptionDate], [CloseDate]) VALUES (110, N'Portfolio 110', CAST(N'2000-08-15 00:00:00.000' AS DateTime), CAST(N'2001-11-04 00:00:00.000' AS DateTime))
+INSERT [Portfolio].[Portfolio] ([Number], [Name], [InceptionDate], [CloseDate]) VALUES (110, N'AAB Investments LLC - Turk', CAST(N'2000-08-15 00:00:00.000' AS DateTime), CAST(N'2001-11-04 00:00:00.000' AS DateTime))
 GO
-INSERT [Portfolio].[Portfolio] ([Number], [Name], [InceptionDate], [CloseDate]) VALUES (111, N'Portfolio 111', CAST(N'1999-07-03 00:00:00.000' AS DateTime), NULL)
+INSERT [Portfolio].[Portfolio] ([Number], [Name], [InceptionDate], [CloseDate]) VALUES (111, N'Dimensional European Small Companies Fund - Class USD (Accumulation)', CAST(N'1999-07-03 00:00:00.000' AS DateTime), NULL)
 GO
-INSERT [Portfolio].[Portfolio] ([Number], [Name], [InceptionDate], [CloseDate]) VALUES (112, N'Portfolio 112', CAST(N'2003-04-09 00:00:00.000' AS DateTime), NULL)
+INSERT [Portfolio].[Portfolio] ([Number], [Name], [InceptionDate], [CloseDate]) VALUES (112, N'Simulated Emerging Markets Portfolio', CAST(N'2003-04-09 00:00:00.000' AS DateTime), NULL)
 GO
-INSERT [Portfolio].[Portfolio] ([Number], [Name], [InceptionDate], [CloseDate]) VALUES (113, N'Portfolio 113', CAST(N'2010-08-09 00:00:00.000' AS DateTime), NULL)
+INSERT [Portfolio].[Portfolio] ([Number], [Name], [InceptionDate], [CloseDate]) VALUES (113, N'Shire PERS Small Cap Value', CAST(N'2010-08-09 00:00:00.000' AS DateTime), NULL)
 GO
-INSERT [Portfolio].[Portfolio] ([Number], [Name], [InceptionDate], [CloseDate]) VALUES (114, N'Portfolio 114', CAST(N'2013-01-03 00:00:00.000' AS DateTime), NULL)
+INSERT [Portfolio].[Portfolio] ([Number], [Name], [InceptionDate], [CloseDate]) VALUES (114, N'Dimensional European Small Companies Fund', CAST(N'2013-01-03 00:00:00.000' AS DateTime), NULL)
 GO
-INSERT [Portfolio].[Portfolio] ([Number], [Name], [InceptionDate], [CloseDate]) VALUES (115, N'Portfolio 115', CAST(N'1985-12-30 00:00:00.000' AS DateTime), NULL)
+INSERT [Portfolio].[Portfolio] ([Number], [Name], [InceptionDate], [CloseDate]) VALUES (115, N'Global Allocation 60/40 Portfolio - Class R2', CAST(N'1985-12-30 00:00:00.000' AS DateTime), NULL)
 GO
-INSERT [Portfolio].[Portfolio] ([Number], [Name], [InceptionDate], [CloseDate]) VALUES (116, N'Portfolio 116', CAST(N'2000-08-15 00:00:00.000' AS DateTime), NULL)
+INSERT [Portfolio].[Portfolio] ([Number], [Name], [InceptionDate], [CloseDate]) VALUES (116, N'Global Allocation 25/75 Portfolio - Class R2', CAST(N'2000-08-15 00:00:00.000' AS DateTime), NULL)
 GO
-INSERT [Portfolio].[Portfolio] ([Number], [Name], [InceptionDate], [CloseDate]) VALUES (117, N'Portfolio 117', CAST(N'1999-07-03 00:00:00.000' AS DateTime), CAST(N'2005-03-07 00:00:00.000' AS DateTime))
+INSERT [Portfolio].[Portfolio] ([Number], [Name], [InceptionDate], [CloseDate]) VALUES (117, N'United Kingdom Small Companies Fund - Income Class (GBP)', CAST(N'1999-07-03 00:00:00.000' AS DateTime), CAST(N'2005-03-07 00:00:00.000' AS DateTime))
 GO
-INSERT [Portfolio].[Portfolio] ([Number], [Name], [InceptionDate], [CloseDate]) VALUES (118, N'Portfolio 118', CAST(N'2003-04-09 00:00:00.000' AS DateTime), NULL)
+INSERT [Portfolio].[Portfolio] ([Number], [Name], [InceptionDate], [CloseDate]) VALUES (118, N'DFA Short-Term Municipal Bond Portfolio-Institutional Class', CAST(N'2003-04-09 00:00:00.000' AS DateTime), NULL)
 GO
-INSERT [Portfolio].[Portfolio] ([Number], [Name], [InceptionDate], [CloseDate]) VALUES (119, N'Portfolio 119', CAST(N'2013-01-03 00:00:00.000' AS DateTime), NULL)
+INSERT [Portfolio].[Portfolio] ([Number], [Name], [InceptionDate], [CloseDate]) VALUES (119, N'Tax-Managed U.S. Equity Series', CAST(N'2013-01-03 00:00:00.000' AS DateTime), NULL)
 GO
-INSERT [Portfolio].[Portfolio] ([Number], [Name], [InceptionDate], [CloseDate]) VALUES (120, N'Portfolio 120', CAST(N'2000-08-15 00:00:00.000' AS DateTime), NULL)
+INSERT [Portfolio].[Portfolio] ([Number], [Name], [InceptionDate], [CloseDate]) VALUES (120, N'Dimensional Short Term Fixed Interest Trust', CAST(N'2000-08-15 00:00:00.000' AS DateTime), NULL)
 GO
 INSERT [Portfolio].[PortfolioToBenchmark] ([PortfolioNumber], [BenchmarkNumber], [SortOrder]) VALUES (101, 10010, 1)
 GO
